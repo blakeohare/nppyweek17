de2696b04692df950990c74fc530478ca7a592c1
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@c9b7a4c0-f2c2-fb08-31e0-79ccee28c19e>
2013-04-14 22:02:27 +0000

    Budget Bar and fonts

--- trunk/supercode/Util.py
+++ trunk/supercode/Util.py
@@ -40,4 +40,59 @@ _sound_id_to_file = {
 def play_sound(id):
 	file = _sound_id_to_file.get(id)
 	# TODO: play that file
-	
+
+
+_letter_lookup = {
+	':': 'colon',
+	',': 'comma',
+	'$': 'dollar',
+	'!': 'excl',
+	'.': 'period',
+	'?': 'ques'
+}
+for c in 'abcdefghijklmnopqrstuvwxyz':
+	_letter_lookup[c] = c
+	_letter_lookup[c.upper()] = c
+for c in range(10):
+	_letter_lookup[str(c)] = 'num' + str(c)
+_letters = {}
+def char_to_img(char):
+	img = _letters.get(char, None)
+	if img == None:
+		if char == ' ':
+			sps = char_to_img('n')
+			img = pygame.Surface(sps.get_size(), pygame.SRCALPHA) # blank image the size of the 'N' image
+		else:
+			t = _letter_lookup.get(char, 'ques')
+			img = get_image('font/' + t)
+		_letters[char] = img
+	return img
+
+def max(a, b):
+	if a > b: return a
+	return b
+
+def min(a, b):
+	if a < b: return a
+	return b
+
+_text_cache = {}
+def get_text(text):
+	img = _text_cache.get(text)
+	if img == None:
+		letters = []
+		width = 0
+		height = 0
+		for char in text:
+			c = char_to_img(char)
+			letters.append(c)
+			width += c.get_width()
+			height = max(height, c.get_height())
+		img = pygame.Surface((width, height), pygame.SRCALPHA)
+		x = 0
+		for char in letters:
+			img.blit(char, (x, 0))
+			x += char.get_width()
+		_text_cache[text] = img
+	return img
+			
