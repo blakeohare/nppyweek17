4869a08a0b91d3146f29b4a4b129133bc955b94d
mspaintmaestro <mspaintmaestro@gmail.com>
2013-04-14 19:20:46 +0000

    Fixed boxes. Introduction of game session logic.

--- supercode/Box.py
+++ supercode/Box.py
@@ -1,10 +1,9 @@
 from supercode.Util import *
 
 class Box:
-	def __init__(self, color, is_good):
-		self.color = color
-		self.is_good = is_good
-		self.img = get_image('boxes/box_' + color)
+	def __init__(self, key):
+		self.key = key
+		self.img = get_image('boxes/' + key)
 	
 	def render(self, screen, left, top, game_x, game_y, height):
 		x = int(left + game_x * 16 - 8)
