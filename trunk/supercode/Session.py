f62915f52ecee39051a8c016a8b12ba4288a4a57
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@c9b7a4c0-f2c2-fb08-31e0-79ccee28c19e>
2013-04-14 20:27:10 +0000

    rudimentary sprite automation

--- trunk/supercode/Session.py
+++ trunk/supercode/Session.py
@@ -1,6 +1,7 @@
 import random
 
 from supercode.Box import *
+from supercode.Sprite import *
 
 all_colors = 'red orange yellow lime green aqua blue purple pink brown'.split()
 polarities = 'h v n'.split()
@@ -8,8 +9,12 @@ polarities = 'h v n'.split()
 STARTING_BUDGET = 1000
 STARTING_ITEM_PRICE = 100
 
+HEROES = 'superman batman'.split()
+VILLAINS = 'joker riddler'.split()
+
 class Session:
 	def __init__(self):
+		self.counter = 0
 		self.balance = 0.5
 		self.reputation = 1.0
 		self.budget = STARTING_BUDGET
@@ -37,3 +42,30 @@ class Session:
 		
 		return Box(key)
 	
+	
+	def update(self):
+		self.counter += 1
+	
+	def check_for_customer(self):
+		
+		if self.counter % (30 * 5) == 0:
+			pass
+		else:
+			return None
+		# on average, every 15 seconds for now
+		# fps is 30
+		# probability of customer is 1 in 15 * 30
+		#if random.random() * 15 * 30 > 1: # TODO: tweak this
+		#	return None
+		
+		is_hero = random.random() < .5
+		
+		list = HEROES if is_hero else VILLAINS
+		
+		key = random.choice(list)
+		
+		sprite = Sprite(key, is_hero, ['n_blue'])
+		
+		return sprite
+		
+		
