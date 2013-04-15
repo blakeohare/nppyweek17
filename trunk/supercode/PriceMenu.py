30593de823eef00cbe3c340b1d0487791ee222a7
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@c9b7a4c0-f2c2-fb08-31e0-79ccee28c19e>
2013-04-15 02:54:17 +0000

    More price menu.

--- trunk/supercode/PriceMenu.py
+++ trunk/supercode/PriceMenu.py
@@ -6,6 +6,7 @@ class PriceMenu:
 	def __init__(self, bg):
 		self.next = self
 		self.bg = bg
+		self.render_cache = None
 	
 	def process_input(self, events, pressed_keys):
 		for event in events:
@@ -22,6 +23,34 @@ class PriceMenu:
 	def render2(self, screen, counter):
 		self.bg.render2(screen, counter)
 		
-		#pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(20, 20, 20, 20))
-		screen.blit(get_image('misc/price_menu_bg'), (122, 152))
-	
+		if self.render_cache == None:
+			left = 0
+			top = 0
+			
+			self.render_cache = get_image('misc/price_menu_bg').copy()
+			
+			margin_left = 38
+			margin_top = 75
+			
+			spacing_x = 67
+			spacing_y = 80
+			
+			prices = self.bg.session.prices
+			
+			x = left + margin_left
+			for color in 'red orange yellow lime green aqua blue purple pink brown'.split():
+				y = top + margin_top
+				for polarity in ('h', 'n', 'v'):
+					key = polarity + '_' + color
+					price = prices[key]
+					img = get_image('boxes/' + key)
+					self.render_cache.blit(img, (x + 5, y))
+					self.render_cache.blit(get_small_text(format_money(price, False)), (x - 15, y + 32))
+					y += spacing_y
+				x += spacing_x
+			
+			img = get_small_text("Left click to raise price. Right click to lower.")
+			x = (self.render_cache.get_width() - img.get_width()) // 2
+			self.render_cache.blit(img, (x, y))
+		
+		screen.blit(self.render_cache, (122, 152))
