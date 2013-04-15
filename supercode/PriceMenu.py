594084fbc40927a6da35bd685ea3940fb983b4d9
mspaintmaestro <mspaintmaestro@gmail.com>
2013-04-15 02:29:14 +0000

    Price menu stub. Tweaked budget bar slightly

---
+++ supercode/PriceMenu.py
@@ -0,0 +1,25 @@
+import pygame
+
+class PriceMenu:
+	def __init__(self, bg):
+		self.next = self
+		self.bg = bg
+	
+	def process_input(self, events, pressed_keys):
+		for event in events:
+			if event.down and event.action == 'full':
+				self.next = self.bg
+				self.bg.next = self.bg
+				
+	
+	def update(self, counter):
+		self.bg.update(counter)
+	
+	def render1(self, screen, counter):
+		self.bg.render1(screen, counter)
+	
+	def render2(self, screen, counter):
+		self.bg.render2(screen, counter)
+		
+		pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(20, 20, 20, 20))
+	
