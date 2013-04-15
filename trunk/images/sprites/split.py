e1f210f807ca953ca9ebcca90a0cd19547e3b5e7
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@c9b7a4c0-f2c2-fb08-31e0-79ccee28c19e>
2013-04-15 03:50:04 +0000

    I forget SVN doesn't automatically check added files.

---
+++ trunk/images/sprites/split.py
@@ -0,0 +1,40 @@
+import pygame
+import os
+
+pygame.init()
+characters = 'player'.split()
+
+for character in characters:
+	img = pygame.image.load(character + '.png')
+
+
+
+	cols = '$_*_down $_*_up $_*_left $_*_right $_hold_*_down $_hold_*_up $_hold_*_left $_hold_*_right'.replace('$', character).split()
+	rows = 'standing walking_1 walking_2 walking_3 walking_4'.split()
+
+	width = 16
+	height = 32
+
+	x = 0
+	for col in cols:
+		y = 0
+		for row in rows:
+			file = None
+			parts = row.split('_')
+			action = parts[0]
+			if len(parts) == 2:
+				num = str(parts[1])
+			else:
+				num = None
+			
+			file = col.replace('*', action)
+			if num != None:
+				file += '_' + num
+			file += '.png'
+			
+			output = pygame.Surface((16, 32), pygame.SRCALPHA)
+			output.blit(img, (-x, -y))
+			print file, output
+			pygame.image.save(output, 'foo\\' + file)
+			y += height
+		x += width
