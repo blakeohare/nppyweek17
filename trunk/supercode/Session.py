73b79231f0b469b1d7c9021f1a564a88b4f6ab46
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@c9b7a4c0-f2c2-fb08-31e0-79ccee28c19e>
2013-04-18 04:23:11 +0000

    Move constants to one file for easy tweaking.

--- trunk/supercode/Session.py
+++ trunk/supercode/Session.py
@@ -2,20 +2,7 @@ import random
 
 from supercode.Box import *
 from supercode.Sprite import *
-
-HEROES = ('superman', 'hero1')
-VILLAINS = ('villain1', 'villain2')
-
-ALL_COLORS = (
-	'red', 'orange', 'yellow', 'lime', 'green',
-	'aqua', 'blue', 'purple', 'pink', 'brown')
-
-POLARITIES = ('h', 'v', 'n')
-
-STARTING_BUDGET = 1000
-STARTING_ITEM_PRICE = 100
-
-NORMAL_CUSTOMER_FREQUENCY = 20 # average seconds between customers when all prices are $100 (calculations for heroes and villains are done individually so this is actually twice as frequent if you consider both halves of the shop)
+from supercode.Tweaks import *
 
 class Session:
 	def __init__(self):
