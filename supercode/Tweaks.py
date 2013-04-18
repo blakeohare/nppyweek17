1604f107c2eeacad0faa194153b0eae7e2a40a9a
mspaintmaestro <mspaintmaestro@gmail.com>
2013-04-18 04:23:11 +0000

    Move constants to one file for easy tweaking.

---
+++ supercode/Tweaks.py
@@ -0,0 +1,14 @@
+
+HEROES = ('superman', 'hero1')
+VILLAINS = ('villain1', 'villain2')
+
+ALL_COLORS = (
+	'red', 'orange', 'yellow', 'lime', 'green',
+	'aqua', 'blue', 'purple', 'pink', 'brown')
+
+POLARITIES = ('h', 'v', 'n')
+
+STARTING_BUDGET = 1000
+STARTING_ITEM_PRICE = 100
+
+NORMAL_CUSTOMER_FREQUENCY = 10 # average seconds between customers when all prices are $100 (calculations for heroes and villains are done individually so this is actually twice as frequent if you consider both halves of the shop)
