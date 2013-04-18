8990f54ed94a0a6e05ee539bf23acf2ba838f265
mspaintmaestro <mspaintmaestro@gmail.com>
2013-04-18 04:16:05 +0000

    Calculate demand and customer rate based on prices.

--- supercode/Session.py
+++ supercode/Session.py
@@ -3,16 +3,19 @@ import random
 from supercode.Box import *
 from supercode.Sprite import *
 
-all_colors = 'red orange yellow lime green aqua blue purple pink brown'.split()
-ALL_COLORS = all_colors # yeah, I'm too lazy to rename all instances of all_colors
+HEROES = ('superman', 'hero1')
+VILLAINS = ('villain1', 'villain2')
 
-polarities = 'h v n'.split()
+ALL_COLORS = (
+	'red', 'orange', 'yellow', 'lime', 'green',
+	'aqua', 'blue', 'purple', 'pink', 'brown')
+
+POLARITIES = ('h', 'v', 'n')
 
 STARTING_BUDGET = 1000
 STARTING_ITEM_PRICE = 100
 
-HEROES = 'superman hero1'.split()
-VILLAINS = 'villain1 villain2'.split()
+NORMAL_CUSTOMER_FREQUENCY = 20 # average seconds between customers when all prices are $100 (calculations for heroes and villains are done individually so this is actually twice as frequent if you consider both halves of the shop)
 
 class Session:
 	def __init__(self):
@@ -23,8 +26,8 @@ class Session:
 		self.spectrum_available = 1
 		self.restocking = []
 		self.prices = {}
-		for color in all_colors:
-			for polarity in polarities:
+		for color in ALL_COLORS:
+			for polarity in POLARITIES:
 				key = polarity + '_' + color
 				self.prices[key] = STARTING_ITEM_PRICE
 		
@@ -60,9 +63,8 @@ class Session:
 		return ALL_COLORS[self.spectrum_available - 1]
 	
 	def get_random_box(self, is_free):
-		# TODO: this function should weight itself on stuff
-		color = random.choice(all_colors[:self.spectrum_available])
-		polarity = random.choice(polarities)
+		color = random.choice(ALL_COLORS[:self.spectrum_available])
+		polarity = random.choice(POLARITIES)
 		
 		key = polarity + '_' + color
 		
@@ -75,44 +77,100 @@ class Session:
 		
 		return Box(key)
 	
-	
 	def update(self):
 		self.counter += 1
 	
-	def check_for_customer(self):
+	def check_for_customers(self):
+		# Do the calculation individually for heros and villains.
+		# Return a list of 1, or 2 customers or None
+		output = None
+		for ih in (True, False):
+			customer = self.check_for_customer_by_polarity(ih)
+			if customer != None:
+				if output == None:
+					output = []
+				output.append(customer)
+		return output
+	
+	def check_for_customer_by_polarity(self, is_hero):
 		
-		if self.counter % (3 * 5) == 0:
-			pass
-		else:
+		# Customer presence is determined in two phases. First
+		# the average price for that customer type is computed.
+		# The average price allure is then computed.
+		
+		# 1 customer appears every 20 seconds on average.
+		# At 30 fps, this is 1 / 600, when all prices are $100.
+		
+		# The allure ratio is multiplied to the denominator and creates
+		# a value X.
+		
+		# If random.random() is less than X, a customer is created.
+		
+		x = 1.0 / (NORMAL_CUSTOMER_FREQUENCY * 30)
+		p = ('h', 'n') if is_hero else ('v', 'n') # polarities relevant
+		colors = ALL_COLORS[:self.spectrum_available]
+		total_price = 0
+		average_price = 0
+		for color in colors:
+			for polarity in p:
+				average_price += 100
+				total_price += self.prices[polarity + '_' + color]
+		
+		allure = 1.0 * average_price / total_price
+		
+		x *= allure
+		
+		if random.random() > x:
 			return None
 		
-		is_hero = random.random() < .5
+		character_key = random.choice(HEROES if is_hero else VILLAINS)
 		
-		list = HEROES if is_hero else VILLAINS
+		# 2 colors or less: 1 demand
+		# 5 colors or less: 1 or 2 demands
+		# 8 colors or less: 1 to 3 demands
+		# 9 or more colors: 2 to 4 demands
+		lc = len(colors)
+		if lc < 3:
+			num_demands = [1]
+		elif lc < 6:
+			num_demands = [1, 2]
+		elif lc < 9:
+			num_demands = [1, 2, 3]
+		else:
+			num_demands = [2, 3, 4]
+		num_demands = random.choice(num_demands)
 		
-		key = random.choice(list)
+		demands = self.get_demands(num_demands, is_hero)
 		
-		available_colors = ALL_COLORS[:self.spectrum_available]
-		
-		demands = []
-		
-		demand_count = int(random.random() * 3) + 1
-		while demand_count > 0:
-			color = random.choice(available_colors)
-			neutral = random.random() < .5
-			if not neutral:
-				if is_hero:
-					k = 'h_' + color
-				else:
-					k = 'v_' + color
-			else:
-				k = 'n_' + color
-			demands.append(k)
-			demand_count -= 1
-		
-		sprite = Sprite(key, is_hero, demands)
+		sprite = Sprite(character_key, is_hero, demands)
 		
 		return sprite
+	
+	def get_price_table(self, is_hero):
+		output = []
+		for color in ALL_COLORS[:self.spectrum_available]:
+			for p in ('hn' if is_hero else 'vn'):
+				k = p + '_' + color
+				allure = 100.0 / self.prices[k]
+				if len(output) == 0:
+					output.append((k, allure))
+				else:
+					output.append((k, output[-1][1] + allure))
+		return output
+	
+	def get_demands(self, num, is_hero):
+		ptable = self.get_price_table(is_hero)
+		max_p = ptable[-1][1]
+		output = []
+		for i in range(num):
+			x = random.random() * max_p
+			for t in ptable:
+				if x <= t[1]:
+					output.append(t[0])
+					break
+		if len(output) == 0: # this should never happen...but just in case...
+			return ['n_red']
+		return output
 	
 	def reject_last_customer(self):
 		pass
