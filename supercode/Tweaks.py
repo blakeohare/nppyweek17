2845c9a39e12c8a2a61fdb461769c34a85af65f0
phiofpi <phiofpi@gmail.com>
2013-04-20 18:05:55 +0000

    Tweaks.py tweaked

--- supercode/Tweaks.py
+++ supercode/Tweaks.py
@@ -11,7 +11,7 @@ POLARITIES = ('h', 'v', 'n')
 STARTING_BUDGET = 1000
 STARTING_ITEM_PRICE = 100
 
-NORMAL_CUSTOMER_FREQUENCY = 10 # average seconds between customers when all prices are $100 (calculations for heroes and villains are done individually so this is actually twice as frequent if you consider both halves of the shop)
+NORMAL_CUSTOMER_FREQUENCY = 8 # average seconds between customers when all prices are $100 (calculations for heroes and villains are done individually so this is actually twice as frequent if you consider both halves of the shop)
 
-BALANCE_MULTIPLIER = 1.002
+BALANCE_MULTIPLIER = 1.001
 BALANCE_SALE_OFFSET = 0.02
