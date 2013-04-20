2c25b29819bf84ba454d6005e2bf9055c9c5b914
mspaintmaestro <mspaintmaestro@gmail.com>
2013-04-20 05:54:23 +0000

    Balance meter moves too fast.

--- supercode/Tweaks.py
+++ supercode/Tweaks.py
@@ -13,5 +13,5 @@ STARTING_ITEM_PRICE = 100
 
 NORMAL_CUSTOMER_FREQUENCY = 10 # average seconds between customers when all prices are $100 (calculations for heroes and villains are done individually so this is actually twice as frequent if you consider both halves of the shop)
 
-BALANCE_MULTIPLIER = 1.003
+BALANCE_MULTIPLIER = 1.002
 BALANCE_SALE_OFFSET = 0.02
