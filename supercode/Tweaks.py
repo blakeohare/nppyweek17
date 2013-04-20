769298d76006169931346fe8cdf82b1193e553ad
phiofpi <phiofpi@gmail.com>
2013-04-20 23:04:55 +0000

    balance mult tweaked

--- supercode/Tweaks.py
+++ supercode/Tweaks.py
@@ -13,7 +13,7 @@ STARTING_ITEM_PRICE = 100
 
 NORMAL_CUSTOMER_FREQUENCY = 10 # average seconds between customers when all prices are $100 (calculations for heroes and villains are done individually so this is actually twice as frequent if you consider both halves of the shop)
 
-BALANCE_MULTIPLIER = 1.001
+BALANCE_MULTIPLIER = 1.0005
 BALANCE_SALE_OFFSET = 0.02
 
 STARTING_GRUNTLES_COUNT = 2000
