22683482a8edf35084a805c7951f24ff6fef641c
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@c9b7a4c0-f2c2-fb08-31e0-79ccee28c19e>
2013-04-20 05:28:47 +0000

    Balance!
    Added constants to tweaks.

--- trunk/supercode/Tweaks.py
+++ trunk/supercode/Tweaks.py
@@ -12,3 +12,6 @@ STARTING_BUDGET = 1000
 STARTING_ITEM_PRICE = 100
 
 NORMAL_CUSTOMER_FREQUENCY = 10 # average seconds between customers when all prices are $100 (calculations for heroes and villains are done individually so this is actually twice as frequent if you consider both halves of the shop)
+
+BALANCE_MULTIPLIER = 1.003
+BALANCE_SALE_OFFSET = 0.02
