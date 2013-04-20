6849957b412b920c419a06623a39629224889bcc
mspaintmaestro <mspaintmaestro@gmail.com>
2013-04-20 05:15:14 +0000

    Pause menu implementation.

--- supercode/PauseMenu.py
+++ supercode/PauseMenu.py
@@ -1,16 +1,41 @@
 import pygame
 
+from supercode.TitleScene import *
+from supercode.Util import *
+
 class PauseMenu:
 	def __init__(self, bg):
 		self.next = self
 		self.bg = bg
 		self.img = None
+		self.index = 0
 	
 	def update(self, counter):
 		pass
 	
 	def process_input(self, events, pressed_keys):
-		pass
+		for event in events:
+			if event.down:
+				if event.action == 'up':
+					if self.index > 0:
+						self.index -= 1
+				elif event.action == 'down':
+					if self.index < 3:
+						self.index += 1
+				elif event.action == 'pause':
+					self.do(self.index)
+	
+	def do(self, index):
+		if index == 0: # resume
+			self.bg.next = self.bg
+			self.next = self.bg
+		elif index == 1: # make funny noise
+			pass # TODO: this
+		elif index == 2: # exit to menu
+			from supercode.TitleScene import TitleScene # dafuq? why will the next line not work even though this is at the top of the file?
+			self.next = TitleScene()
+		elif index == 3: # exit the game
+			self.next = None # TODO: simple exit screen like Katamari Damacy?
 	
 	def render1(self, screen, rcounter):
 		pass
@@ -22,6 +47,31 @@ class PauseMenu:
 			self.img = pygame.Surface(screen.get_size())
 			pygame.transform.scale(s1, screen.get_size(), self.img)
 			self.bg.render2(self.img, rcounter)
+			dark = pygame.Surface(screen.get_size()).convert()
+			dark.fill((0, 0, 0,))
+			dark.set_alpha(180)
+			self.img.blit(dark, (0, 0))
+			menu = get_image('misc/price_menu_bg')
+			x = (screen.get_width() - menu.get_width()) // 2
+			y = (screen.get_height() - menu.get_height()) // 2
+			self.img.blit(menu, (x, y))
+			text = get_text("Paused!")
+			x += 30
+			y += 30
+			self.img.blit(text, (x, y))
+			
+			options = ["Resume", "Make funny noise", "Quit to Menu", "Exit to DOS"]
+			x += 90
+			y += text.get_height() + 55
+			
+			self.option_loc = []
+			for option in options:
+				self.option_loc.append((x, y))
+				txt = get_text(option)
+				self.img.blit(txt, (x + 30, y))
+				y += txt.get_height() + 20
+			
 		screen.blit(self.img, (0, 0))
 		
-		pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(80, 80, screen.get_width() - 80 * 2, screen.get_height() - 80 * 2))
+		cursor = self.option_loc[self.index]
+		screen.blit(get_arrow('right', True, rcounter // 3), (cursor[0] - 15, cursor[1] - 10))
